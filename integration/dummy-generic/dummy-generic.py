4b31f0bcd98b007341063e08612a0f0f6c736546
Alexandre Froger <alex@froger.me>
2024-01-10 14:35:45 +0800

    generic py

---
+++ integration/dummy-generic/dummy-generic.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+# pylint: disable=C0103
+"""
+Dummy Generic
+"""
+import importlib
+import sys
+import json
+
+wppus_api = importlib.import_module("wppus-api")
+
+def status():
+    """
+    ### CHECKING THE PACKAGE STATUS ###
+    """
+    if wppus_api.is_installed():
+        print("Status: Installed")
+    elif not wppus_api.is_installed():
+        print("Status: Not installed")
+    else:
+        print("Status: Unknown")
+
+def install(license_key):
+    """
+    ### INSTALLING THE PACKAGE ###
+    """
+    # If the command is "install", the script is not installed, and the license key is not empty
+    if not wppus_api.is_installed() and license_key:
+        # Install the script
+        wppus_api.install(license_key)
+
+        print("Installed")
+    else:
+        print("Failed to install")
+
+def uninstall():
+    """
+    ### UNINSTALLING THE PACKAGE ###
+    """
+    # If the command is "uninstall" and the script is installed
+    if wppus_api.is_installed():
+        # Uninstall the script
+        wppus_api.uninstall()
+
+        print("Uninstalled")
+    else:
+        print("Failed to uninstall")
+
+def activate():
+    """
+    ### ACTIVATING THE LICENSE ###
+    """
+    # If the command is "activate", the script is installed, and the license key is not empty
+    if wppus_api.is_installed() :
+        # Activate the license
+        wppus_api.activate_license()
+
+        print("Activated")
+    else:
+        print("The package is not installed")
+
+def deactivate():
+    """
+    ### DEACTIVATING THE LICENSE ###
+    """
+    # If the command is "deactivate" and the script is installed
+    if wppus_api.is_installed():
+        # Deactivate the license
+        wppus_api.deactivate_license()
+
+        print("Deactivated")
+    else:
+        print("The package is not installed")
+
+def get_update_info():
+    """
+    ### GETTING UPDATE INFORMATION ###
+    """
+    # If the command is "get_update_info" and the script is installed
+    if wppus_api.is_installed():
+        # Get the update information
+        info = wppus_api.get_update_info()
+        # Get the current version
+        version = wppus_api.get_version()
+        # Get the remote version
+        new_version = info["version"]
+
+        print("")
+        # Get the current version
+        print("current " + version + " vs. remote " + new_version)
+        if new_version > version:
+            print("")
+            print("---------")
+            print("")
+            print("Update available !!! Run the \"update\" command!")
+        print("")
+        print("---------")
+        print("")
+        # Pretty print the response
+        print(json.dumps(wppus_api.get_update_info(), indent=4))
+        print("")
+    else:
+        print("The package is not installed")
+
+def update():
+    """
+    ### UPDATING THE PACKAGE ###
+    """
+    # If the command is "update" and the script is installed
+    if wppus_api.is_installed():
+        # Get the update information
+        wppus_api.update()
+
+        print("Updated")
+        print("")
+        # Pretty print the response
+        print(json.dumps(wppus_api.get_update_info(), indent=4))
+    else:
+        print("The package is not installed")
+
+def usage():
+    """
+    ### USAGE ###
+    """
+    print("Usage: ./dummy-generic.py [command] [arguments]")
+    print("Commands:")
+    print("  install [license] - install the package")
+    print("  uninstall - uninstall the package")
+    print("  activate - activate the license")
+    print("  deactivate - deactivate the license")
+    print("  get_update_info - output information about the remote package update")
+    print("  update - update the package if available")
+    print("  status - output the package status")
+    print("Note: this package assumes it needs a license.")
+
+def main():
+    """
+    ### MAIN ###
+    """
+    funcs = globals()
+
+    if len(sys.argv) > 1 and len(sys.argv) <= 3:
+        command = sys.argv[1]
+        license_key = ''
+
+        if len(sys.argv) == 3:
+            license_key = sys.argv[2]
+
+        if command in funcs:
+
+            if command == "install":
+                funcs["install"](license_key)
+            else:
+                funcs[command]()
+        else:
+            funcs["usage"]()
+    else:
+        funcs["usage"]()
+
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
---
+++ integration/dummy-generic/wppus-api.py
@@ -0,0 +1,282 @@
+# pylint: disable=C0103
+"""
+### EXAMPLE INTEGRATION WITH WP PACKAGES UPDATE SERVER ###
+
+DO NOT USE THIS FILE AS IT IS IN PRODUCTION !!!
+It is just a collection of basic functions and snippets, and they do not
+perform the necessary checks to ensure data integrity ; they assume that all
+the requests are successful, and do not check paths or permissions.
+They also assume that the package necessitates a license key.
+
+replace https://server.domain.tld/ with the URL of the server where
+WP Packages Update Server is installed in wppus.json
+"""
+
+import os
+import sys
+import json
+import subprocess
+import urllib.parse
+import urllib.request
+import urllib.error
+import zipfile
+import shutil
+import tempfile
+import requests
+
+conf = "wppus.json"
+
+# get the config from wppus.json in a variable
+with open(os.path.join(os.path.dirname(__file__), conf), encoding="utf-8") as config_file:
+    wppus_config = json.load(config_file)
+
+# define the url of the server
+url = wppus_config["server"]
+# define the package name
+package_name = os.path.basename(os.path.dirname(__file__))
+# define the package script
+package_script = os.path.join(os.path.dirname(__file__), os.path.basename(__file__))
+# define the current script name
+script_name = os.path.basename(__file__)
+# define the update zip archive name
+zip_name = package_name + ".zip"
+# define the current version of the package from the wppus.json file
+version = wppus_config["packageData"]["Version"]
+
+# define license_key from the wppus.json file - check if it exists to avoid errors
+if "licenseKey" in wppus_config:
+    license_key = wppus_config["licenseKey"]
+else:
+    license_key = ""
+
+# define license_signature from the wppus.json file - check if it exists to avoid errors
+if "licenseSignature" in wppus_config:
+    license_signature = wppus_config["licenseSignature"]
+else:
+    license_signature = ""
+
+# define the domain
+if sys.platform == "darwin":
+    # macOS
+    command = ["ioreg", "-rd1", "-c", "IOPlatformExpertDevice"]
+    domain = subprocess.check_output(command).decode("utf-8")
+elif sys.platform == "linux":
+    # Ubuntu
+    domain = subprocess.check_output(["cat", "/var/lib/dbus/machine-id"]).decode("utf-8").strip()
+
+
+def install():
+    """
+    ### INSTALLING THE PACKAGE ###
+    """
+    # add the license key to wppus.json
+    wppus_config["licenseKey"] = license_key
+
+    # add a file '.installed' in current directory
+    with open(os.path.join(os.path.dirname(__file__), ".installed"), "w", encoding="utf-8") as _:
+        pass
+
+def uninstall():
+    """
+    ### UNINSTALLING THE PACKAGE ###
+    """
+    # remove the license key from wppus.json
+    wppus_config.pop("licenseKey", None)
+    # remove the license signature from wppus.json
+    wppus_config.pop("licenseSignature", None)
+
+    # remove the file '.installed' from current directory
+    os.remove(os.path.join(os.path.dirname(__file__), ".installed"))
+
+
+def is_installed():
+    """
+    ### CHECKING IF THE PACKAGE IS INSTALLED ###
+    """
+    # check if the file '.installed exists in current directory
+    if os.path.isfile(os.path.join(os.path.dirname(__file__), ".installed")):
+
+        # return true
+        return True
+
+    # return false
+    return False
+
+
+def _send_api_request(endpoint, args):
+    """
+    ### SENDING AN API REQUEST ###
+    """
+    # build the request url
+    full_url = url.rstrip('/') + "/" + endpoint + "/?" + "&".join(args)
+
+    # set headers
+    headers = {
+        "user-agent": "curl",
+        "accept": "*/*"
+    }
+
+    # make the request
+    response = requests.get(full_url, headers=headers, timeout=20, verify=True)
+
+    # return the response
+    return response.text
+
+def _check_for_updates():
+    """
+    ### CHECKING FOR UPDATES ###
+    """
+    # build the request url
+    endpoint = "wppus-update-api"
+    args = [
+        "action=get_metadata",
+        "package_id=" + urllib.parse.quote(package_name),
+        "installed_version=" + urllib.parse.quote(version),
+        "license_key=" + urllib.parse.quote(license_key),
+        "license_signature=" + urllib.parse.quote(license_signature),
+        "update_type=Generic"
+    ]
+    # make the request
+    response = _send_api_request(endpoint, args)
+
+    # return the response
+    return response
+
+
+def activate_license():
+    """
+    ### ACTIVATING A LICENSE ###
+    """
+    # build the request url
+    endpoint = "wppus-license-api"
+    args = [
+        "action=activate",
+        "license_key=" + urllib.parse.quote(license_key),
+        "allowed_domains=" + urllib.parse.quote(domain),
+        "package_slug=" + urllib.parse.quote(package_name)
+    ]
+    # make the request
+    response = _send_api_request(endpoint, args)
+    # get the signature from the response
+    signature = urllib.parse.unquote(json.loads(response)["license_signature"])
+    # add the license signature to wppus.json
+    wppus_config["licenseSignature"] = signature
+
+    with open(os.path.join(os.path.dirname(__file__), conf), "w", encoding="utf-8") as f:
+        json.dump(wppus_config, f, indent=4)
+
+def deactivate_license():
+    """
+    ### DEACTIVATING A LICENSE ###
+    """
+    # build the request url
+    endpoint = "wppus-license-api"
+    args = [
+        "action=deactivate",
+        "license_key=" + urllib.parse.quote(license_key),
+        "allowed_domains=" + urllib.parse.quote(domain),
+        "package_slug=" + urllib.parse.quote(package_name)
+    ]
+    # make the request
+    _send_api_request(endpoint, args)
+    # remove the license signature from wppus.json
+    wppus_config.pop("licenseSignature", None)
+
+    with open(os.path.join(os.path.dirname(__file__), conf), "w", encoding="utf-8") as f:
+        json.dump(wppus_config, f, indent=4)
+
+def _download_update():
+    """
+    ### DOWNLOADING THE PACKAGE ###
+    """
+    # build the request url
+    endpoint = "wppus-update-api"
+    args = [
+        "action=get_download_url",
+        "package_id=" + urllib.parse.quote(package_name),
+        "installed_version=" + urllib.parse.quote(version),
+        "license_key=" + urllib.parse.quote(license_key),
+        "license_signature=" + urllib.parse.quote(license_signature),
+        "update_type=Generic"
+    ]
+    # make the request
+    response = _send_api_request(endpoint, args)
+    # get the download url from the response
+    _url = urllib.parse.unquote(json.loads(response)["download_url"])
+    # set the path to the downloaded file
+    output_file = os.path.join(tempfile.gettempdir(), zip_name)
+
+    # make the request
+    urllib.request.urlretrieve(_url, output_file)
+
+    # return the path to the downloaded file
+    return output_file
+
+def get_version():
+    """
+    ### GETTING THE PACKAGE VERSION ###
+    """
+    # return the current version of the package
+    return version
+
+def update():
+    """
+    ### UPDATING THE PACKAGE ###
+    """
+    # check for updates
+    response = _check_for_updates()
+    # get the version from the response
+    new_version = json.loads(response)["version"]
+
+    if new_version > version:
+        # download the update
+        output_file = _download_update()
+
+        # extract the zip in /tmp/$(package_name)
+        with zipfile.ZipFile(output_file, "r") as zip_file:
+            zip_file.extractall(os.path.join(tempfile.gettempdir(), package_name))
+
+        # get the permissions of the old file
+        octal_mode = oct(os.stat(package_script).st_mode)[-4:]
+        # set the permissions of the new file to the permissions of the old file
+        os.chmod(os.path.join(tempfile.gettempdir(), package_name, script_name), int(octal_mode, 8))
+
+        # move the updated main script to the current directory ;
+        # the updated main script is in charge of overriding the update
+        # scripts by moving files around after update
+        for file in os.listdir(os.path.join(tempfile.gettempdir(), package_name)):
+
+            if file != script_name:
+                src = os.path.join(tempfile.gettempdir(), package_name, file)
+                dest = os.path.join(os.path.dirname(__file__), file)
+
+                if os.path.exists(dest):
+                    os.remove(dest)
+                shutil.move(src, dest)
+
+        # add the license key to wppus.json
+        wppus_config["licenseKey"] = license_key
+
+        with open(os.path.join(os.path.dirname(__file__), conf), "w", encoding="utf-8") as f:
+            json.dump(wppus_config, f, indent=4)
+        # add the license signature to wppus.json
+        wppus_config["licenseSignature"] = license_signature
+
+        with open(os.path.join(os.path.dirname(__file__), conf), "w", encoding="utf-8") as f:
+            json.dump(wppus_config, f, indent=4)
+
+        # remove the directory
+        for file in os.listdir(os.path.join(tempfile.gettempdir(), package_name)):
+            os.remove(os.path.join(tempfile.gettempdir(), package_name, file))
+
+        os.rmdir(os.path.join(tempfile.gettempdir(), package_name))
+        # remove the zip
+        os.remove(output_file)
+
+def get_update_info():
+    """
+    ### GETTING THE PACKAGE INFO ###
+    """
+    response = _check_for_updates()
+    # get the update information - json parsed, knowing it may contain url encoded characters
+    return json.loads(urllib.parse.unquote(response))
